a2a26cfda74ed7825a3bcd943bdd55f02914df71
Raghu-118 <nrraja6621@gmail.com>
2024-11-18 21:18:06 +0530

    Add files via upload

---
+++ pgm2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+class Student1{
+    String usn;
+    String name;
+    String branch;
+    String phone;
+   void read()
+   {
+	   @SuppressWarnings("resource")
+    Scanner s=new Scanner(System.in);
+	   System.out.println("Enter usn");
+	    usn=s.next();
+	    System.out.println("Enter name");
+	    name=s.next();
+	    System.out.println("Enter branch");
+	    branch=s.next();
+	    System.out.println("Enter phone");
+	    phone=s.next();
+	   
+   }
+    // Method to display student details
+    void display() {
+        System.out.println("USN: " + usn);
+        System.out.println("Name: " + name);
+        System.out.println("Branch: " + branch);
+        System.out.println("Phone: " + phone);
+        System.out.println(); // Empty line for better formatting
+    }
+}
+public class pgm2 {
+    public static void main(String[] args) {
+    	Scanner s=new Scanner(System.in);
+        System.out.println("Enter number of students");
+        int n=s.nextInt();
+        Student1[] s1=new Student1[n];
+        for (int i = 0; i < n; i++) 
+        {
+           s1[i] = new Student1();
+        }
+        for(int i=0;i<n;i++)
+        {
+        	s1[i].read();
+        }
+        for(int i=0;i<n;i++)
+        {
+        	s1[i].display();
+        }     
+        s.close();
+    }
+}
